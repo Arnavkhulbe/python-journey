8e8a3e5e295f685f257ca0d687381aec697abb8a
Arnav Khulbe <arnavkhulbe@gmail.com>
2025-10-29 22:59:33 +0530

    Modified Blackjack game with new logic

--- blackjack-game/main.py
+++ blackjack-game/main.py
@@ -1,63 +1,70 @@
-import random
-user=[]
-dealer=[]
-def deal_card():
-    num=[11,2,3,4,5,6,7,8,9,10,10,10,10]
-    choice=random.choice(num)
-    return choice
 
-for i in range(2):
-    user.append(deal_card())
-    dealer.append(deal_card())
+import random
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
 def calculate_score(cards):
-    if sum(cards)==21 and len(cards)==2:
+    score=sum(cards)
+    if score==21 and len(cards)==2: 
         return 0
-    if 11 in cards and sum(cards)>21:
+    elif 11 in cards and score>21:
         cards.remove(11)
         cards.append(1)
-    return sum(cards)
-game_over=False
-#Hint 11: The score will need to be rechecked with every new card drawn and the
-#checks in Hint 9 need to be repeated until the game ends.
-while not game_over:
-    #Hint 9: Call calculate_score(). If the computer or the user has a blackjack
-    #(0) or if the user's score is over 21, then the game ends.
-    out1=calculate_score(user)
-    out2=calculate_score(dealer)
-    print(f"your cards are{user} and score is {out1}")
-    print(f"dealer first card is {dealer[0]}")
-    if out1==0 or out2==0 or out1>21:
-        game_over=True
-    #Hint 10: If the game has not ended, ask the user if they want to draw another
-    #card. If yes, then use the deal_card() function to add another card to the
-    #user_cards List. If no, then the game has ended.
-    if game_over==False:
-        choice1=input("do you want to add more('yes' or 'no')").lower()
-        if choice1=="yes":
-            user.append(deal_card())  
-            print(f"your score is now {calculate_score(user)}")
-        elif choice1=="no":
-            game_over=True
-#Hint 12: Once the user is done, it's time to let the computer play. The computer
-#should keep drawing cards as long as it has a score less than 17.
-while out2!=21 and out2<17:
-    dealer.append(deal_card())
-    out2=calculate_score(dealer)
-    print(f"your final cards are{user} and xcore being {out1}")
-    print(f"comp cards are{dealer} and xcore being {out2}")
-def compare(out1,out2):
-    if out1==out2:
-        print("draw")
-    elif out1==0:
-        print("user wins")
-    elif out2==0:
-        print("you lost ")
-    elif out1>21:
-        print("you lost ")
-    elif out2>21:
-        print("you won")
-    elif out1>out2:
-        print("you won")
+        return sum(cards)    
     else:
-        print("you lost ")
-compare(out1,out2) 
+        return score
+def compare(user_score,dealer_score):
+    if user_score==dealer_score:
+        return "its a draw"
+    elif dealer_score==0:
+        return "you lose"
+    elif user_score==0:
+        return "you won"
+    elif user_score>21:
+        return "you lose"
+    elif dealer_score>21:
+        return "you won"
+    else:
+        if user_score>dealer_score:
+            return "you won"
+        else:
+            return "you lose"
+case1=True
+while case1:
+    user=[]
+    dealer=[]
+    for i in range(2):
+        user.append(deal_card())
+        dealer.append(deal_card())
+    #print(user)
+    
+    case=True
+    while case:
+        user_score=calculate_score(user)
+        dealer_score=calculate_score(dealer)
+        print(f" your score is {user_score}, your cards: {user}")
+        print(f" computer first card: {dealer[0]}")
+        if user_score==0 or dealer_score==0:
+            case=False
+        elif user_score>21:
+            case=False
+        elif user_score<21:
+            ans=input("do you want to add another card...yes or no?")
+            if ans=="yes":
+                user.append(deal_card())
+            elif ans=="no":
+                case=False
+        user_score=calculate_score(user) 
+    if user_score!=0 and user_score<=21:     
+        while dealer_score<17:        
+            dealer.append(deal_card())
+            dealer_score=calculate_score(dealer)
+    
+    result=compare(user_score,dealer_score) 
+    print(result)
+    ans1=input("do you want to play again or not...yes or no?")
+    if ans1=="yes":
+        case1=True
+    else:
+        case1=False
+        print("game ended")
